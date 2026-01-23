deaf2ac7a4817ebe84ed8a17cfe85de52b305008
studentbcit09 <256634811+studentbcit09@users.noreply.github.com>
2026-01-23 12:22:31 -0800

    changes after testing -- functional version
    change logic to fix listscan() issue (wiped the results of the scan from portscanner)
    
    fixed formatting issues
    
    move no open port error handling

--- lab2/scanner.py
+++ lab2/scanner.py
@@ -1,4 +1,8 @@
-import nmap
+try:
+    import nmap
+except ImportError:
+    print('Missing nmap python module. Please install and try again.')
+    exit()
 
 def port_scanning():
     port_scanner = nmap.PortScanner()
@@ -12,36 +16,35 @@ def port_scanning():
 
     port_range = port_start.strip() + '-' + port_end.strip()
     results = port_scanner.scan(host, port_range, '-sV')
-
+    
+    print(port_scanner.command_line())
+    
     # Includes failure to resolve hostname/invalid IP
     if 'error' in port_scanner.scaninfo():
         print("Error occured while scanning: %s" % port_scanner.scaninfo()['error'][0])
         return
 
     # Host is unreachable or scannable
-    if not port_scanner.listscan():
+    if not port_scanner.all_hosts():
         print("Error: Unable to reach host")
+        return
     
-    host_info = port_scanner[port_scanner.listscan()[0]]
+    host_info = port_scanner[port_scanner.all_hosts()[0]]
     print('host: %s (%s)' % (host, host_info.hostname()))
     print('state: %s' % host_info.state())
-    
-    for protocol in host_info.all_protocols():
-        print('Protocol: %s' % protocol)
+    if not host_info.all_protocols():
+        print("No open ports for %s" % host)
+    else:
+        for protocol in host_info.all_protocols():
+            print('Protocol: %s' % protocol)
 
-        port_status = host_info[protocol].keys()
-        if not port_status:
-            print('No open %s ports' % protocol)
-        else:
-            port_status.sort()
-
+            port_status = host_info[protocol].keys()
             for port in port_status:
                 port_info = host_info[protocol][port]
-                # if 'open' == state:
                 if not port_info['name']:
-                    print('port: %s, state: %s' % port, port_info['state'])
+                    print('port: %s\t state: %s\t' % (port, port_info['state']))
                 else:
-                    print('port: %s state: %s service: %s' % (port, port_info['state'], port_info['name']))
+                    print('port: %s\t state: %s\t service: %s' % (port, port_info['state'], port_info['name']))
 
 if __name__ == '__main__':
     port_scanning()
